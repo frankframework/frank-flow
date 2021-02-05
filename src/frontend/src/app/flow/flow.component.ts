b235d15540511e40e03633004e54553e439dab77
Sergi Philipsen <philipsen.sergi@gmail.com>
2021-02-05 11:40:50 +0100

    Add flow module with a canvas and palette Add search and group for palette

---
+++ src/frontend/src/app/flow/flow.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-flow',
+  templateUrl: './flow.component.html',
+  styleUrls: ['./flow.component.scss']
+})
+export class FlowComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/frontend/src/app/flow/palette/palette.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PaletteComponent } from './palette.component';
+
+describe('PaletteComponent', () => {
+  let component: PaletteComponent;
+  let fixture: ComponentFixture<PaletteComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PaletteComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PaletteComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
