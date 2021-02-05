aedc0ed38715eb7483bb6ea89f36ade497def3c0
Sergi Philipsen <philipsen.sergi@gmail.com>
2021-02-05 12:48:53 +0100

    Remove old frontend

--- src/frontend/test/Mock/FlowModelMock.js
+++
@@ -1,9 +0,0 @@
-import FlowModel from "../../app/Model/FlowModel";
-
-export default class FlowModelMock {
-    constructor() {
-        this.flowModel = new FlowModel();
-
-        return this.flowModel
-    }
-}
