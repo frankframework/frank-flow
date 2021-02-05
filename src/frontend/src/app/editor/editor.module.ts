842f60b84cfa14b6e9ba58695ba9eb8c915dcd91
Sergi Philipsen <philipsen.sergi@gmail.com>
2021-02-05 11:38:46 +0100

    Add editor and file explorer

---
+++ src/frontend/src/app/editor/editor.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { EditorComponent } from './editor.component';
+import {MonacoEditorModule} from "ngx-monaco-editor";
+import {FormsModule} from "@angular/forms";
+import {TreeviewModule} from "ngx-treeview";
+import { ExplorerComponent } from './explorer/explorer.component';
+
+
+
+@NgModule({
+  declarations: [EditorComponent, ExplorerComponent],
+  exports: [
+    EditorComponent
+  ],
+  imports: [
+    CommonModule,
+    MonacoEditorModule,
+    FormsModule,
+    TreeviewModule
+  ]
+})
+export class EditorModule { }
