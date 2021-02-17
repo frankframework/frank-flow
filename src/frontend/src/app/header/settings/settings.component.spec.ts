e4e3eefcbeb71f929d8c2236c0cb098ecef9f8aa
diedekerkhof <diedekerkhof@gmail.com>
2021-02-17 16:10:49 +0100

    Remove test because settings is now inteface

--- src/frontend/src/app/header/settings/settings.component.spec.ts
+++ src/frontend/src/app/header/settings/settings.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SettingsComponent } from './settings.component';
+import { NgxSmartModalComponent, NgxSmartModalModule } from 'ngx-smart-modal';
 
 describe('SettingsComponent', () => {
   let component: SettingsComponent;
@@ -8,7 +9,8 @@ describe('SettingsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [SettingsComponent],
+      declarations: [SettingsComponent, NgxSmartModalComponent],
+      imports: [NgxSmartModalModule.forChild()],
     }).compileComponents();
   });
 
--- src/frontend/src/app/header/settings/settings.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Settings } from './settings';
-
-describe('Settings', () => {
-  it('should create an instance', () => {
-    expect(new Settings()).toBeTruthy();
-  });
-});
