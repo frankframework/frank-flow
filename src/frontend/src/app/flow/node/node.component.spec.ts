2facaf4da18ba185e7e11b9b0bd35c0a9a7766bb
RUBEN\eigenaar <rbndeheer@gmail.com>
2021-03-08 17:34:17 +0100

    fix test

--- src/frontend/src/app/flow/node/node.component.spec.ts
+++ src/frontend/src/app/flow/node/node.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NodeComponent } from './node.component';
 import { Node } from './Nodes/node';
+import Pipe from './Nodes/pipe';
 import { jsPlumb } from 'jsplumb';
 import { NgxSmartModalModule } from 'ngx-smart-modal';
 
@@ -20,8 +21,8 @@ describe('NodeComponent', () => {
     fixture = TestBed.createComponent(NodeComponent);
     component = fixture.componentInstance;
 
-    component.node = { id: 'TestNode', top: 0, left: 20 } as Node;
-    fixture.elementRef.nativeElement.id = component.node.id;
+    component.node = new Pipe('TestPipe', undefined, 20, 20);
+    fixture.elementRef.nativeElement.id = component.node.getId();
     component.jsPlumbInstance = jsPlumb.getInstance();
 
     fixture.detectChanges();
