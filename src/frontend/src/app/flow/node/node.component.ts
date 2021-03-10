1b30a8c5a178e635912965a4e9e47988cebaa229
Sergi Philipsen <philipsen.sergi@gmail.com>
2021-03-10 18:02:30 +0100

    Change folder name

--- src/frontend/src/app/flow/node/node.component.ts
+++ src/frontend/src/app/flow/node/node.component.ts
@@ -5,7 +5,7 @@ import {
   HostBinding,
   HostListener,
 } from '@angular/core';
-import { Node } from './Nodes/node';
+import { Node } from './nodes/node';
 import { EndpointOptions, jsPlumbInstance } from 'jsplumb';
 import { NgxSmartModalService } from 'ngx-smart-modal';
 
