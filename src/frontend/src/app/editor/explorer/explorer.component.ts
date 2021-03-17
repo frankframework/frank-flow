5d32413d5fb9772969b4811c3871fafa8082b76c
Sergi Philipsen <philipsen.sergi@gmail.com>
2021-03-17 16:59:13 +0100

    Remove isExpanded for subfolders

--- src/frontend/src/app/editor/explorer/explorer.component.ts
+++ src/frontend/src/app/editor/explorer/explorer.component.ts
@@ -60,7 +60,6 @@ export class ExplorerComponent implements AfterViewInit {
       } else {
         items.push({
           label: key,
-          isExpanded: true,
           items: this.parseFiles(configuration, content[key], key + '/'),
         });
       }
