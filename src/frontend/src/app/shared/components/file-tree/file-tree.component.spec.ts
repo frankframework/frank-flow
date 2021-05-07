d39bf4041a1d3698760f9dc47f137b2665e28621
Sergi Philipsen <philipsen.sergi@gmail.com>
2021-05-07 12:40:15 +0200

    Add prettier command for Ruben

--- src/frontend/src/app/shared/components/file-tree/file-tree.component.spec.ts
+++ src/frontend/src/app/shared/components/file-tree/file-tree.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ToastrModule } from 'ngx-toastr';
 
 import { FileTreeComponent } from './file-tree.component';
-import {NgxSmartModalModule} from 'ngx-smart-modal';
+import { NgxSmartModalModule } from 'ngx-smart-modal';
 
 describe('FileTreeComponent', () => {
   let component: FileTreeComponent;
