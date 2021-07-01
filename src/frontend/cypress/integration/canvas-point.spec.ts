11afacd91f4bb69c1b38a5f90ff3c717e8b3021c
Martijn Dirkse <martijn@integrationpartners.nl>
2021-07-01 17:41:07 +0200

    Dont jeopardize logic of distributions

--- src/frontend/cypress/integration/canvas-point.spec.ts
+++ src/frontend/cypress/integration/canvas-point.spec.ts
@@ -1,9 +1,9 @@
-import { CanvasPoint } from './cancas-point';
+import { CanvasPoint } from '../support/cancas-point';
 
 describe('CanvasPoint tests', function () {
   it('Points are close when x and y diffs small positive', function () {
     const first = new CanvasPoint(10, 10);
     const second = new CanvasPoint(11, 11);
-    expect(first.closeTo(second)).toBeTrue();
+    expect(first.closeTo(second)).to.be.true;
   });
 });
