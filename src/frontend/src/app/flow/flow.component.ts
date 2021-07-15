716d9000543ad8f945e20b8d946705f7af8757c5
chiefruds <rbndeheer@gmail.com>
2021-07-15 12:43:41 +0200

    expand canvas on pan

--- src/frontend/src/app/flow/flow.component.ts
+++ src/frontend/src/app/flow/flow.component.ts
@@ -1,24 +1,86 @@
-import { Component } from '@angular/core';
-import { NodeService } from './node/node.service';
-import { PanZoomConfig, PanZoomConfigOptions } from 'ngx-panzoom';
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  Renderer2,
+  ViewChild,
+  ViewContainerRef,
+} from '@angular/core';
+import {
+  PanZoomConfig,
+  PanZoomConfigOptions,
+  PanZoomAPI,
+  PanZoomModel,
+} from 'ngx-panzoom';
+import { Subscription } from 'rxjs';
+import { NodeGeneratorService } from '../shared/services/node-generator.service';
 
 @Component({
   selector: 'app-flow',
   templateUrl: './flow.component.html',
   styleUrls: ['./flow.component.scss'],
 })
-export class FlowComponent {
+export class FlowComponent implements OnInit {
   nodes = [];
   connections = [];
   private panZoomConfigOptions: PanZoomConfigOptions = {
     zoomLevels: 10,
-    zoomStepDuration: 10.2,
+    zoomStepDuration: 0.2,
     freeMouseWheelFactor: 0.01,
     zoomToFitZoomLevelFactor: 0.5,
     dragMouseButton: 'left',
-    keepInBounds: true,
+    zoomOnDoubleClick: false,
   };
   panzoomConfig: PanZoomConfig = new PanZoomConfig(this.panZoomConfigOptions);
+  private modelChangedSubscription!: Subscription;
+  private panZoomAPI!: PanZoomAPI;
+  private apiSubscription!: Subscription;
 
-  constructor(private nodeService: NodeService) {}
+  @ViewChild('nodeContainer', { read: ElementRef })
+  nodeContainerRef!: ElementRef;
+
+  constructor(
+    private nodeGeneratorService: NodeGeneratorService,
+    private renderer: Renderer2
+  ) {}
+
+  ngOnInit(): void {
+    console.log('hoi?');
+    this.modelChangedSubscription = this.panzoomConfig.modelChanged.subscribe(
+      (model: PanZoomModel) => this.onModelChanged(model)
+    );
+    this.apiSubscription = this.panzoomConfig.api.subscribe(
+      (api: PanZoomAPI) => (this.panZoomAPI = api)
+    );
+  }
+
+  onModelChanged(model: PanZoomModel): void {
+    // console.log('change in model: ', model);
+    const offset = 500;
+
+    if (this.nodeContainerRef) {
+      if (model.pan.x >= 0) {
+        this.nodeGeneratorService.offX = offset;
+        //this.panZoomAPI.resetView();
+        this.panZoomAPI.centerContent(10);
+        console.log(
+          'expand canvas: ',
+          this.nodeContainerRef.nativeElement.getElementsByClassName(
+            'canvas'
+          )[0].offsetWidth
+        );
+        const el = this.nodeContainerRef.nativeElement.getElementsByClassName(
+          'canvas'
+        )[0];
+        const elWidth = this.nodeContainerRef.nativeElement.getElementsByClassName(
+          'canvas'
+        )[0].offsetWidth;
+        this.renderer.setStyle(el, 'width', elWidth + offset + 'px');
+      } else if (model.pan.y >= 0) {
+        this.nodeGeneratorService.offY = offset;
+        this.nodeContainerRef.nativeElement.style.height =
+          this.nodeContainerRef.nativeElement.style.height + offset + 'px';
+      }
+    }
+  }
 }
