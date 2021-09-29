4f0d6f9df151751e68f6bc570fa666234d8c339e
Sergi Philipsen <philipsen.sergi@gmail.com>
2021-09-29 11:58:36 +0200

    Lock palette if errors are found in the xml (#212)

--- frank-flow/src/frontend/src/app/flow/palette/palette.component.ts
+++ frank-flow/src/frontend/src/app/flow/palette/palette.component.ts
@@ -1,13 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PaletteService } from './palette.service';
+import { FlowStructureService } from '../../shared/services/flow-structure.service';
 
 @Component({
   selector: 'app-palette',
   templateUrl: './palette.component.html',
   styleUrls: ['./palette.component.scss'],
 })
-export class PaletteComponent {
-  search!: string;
+export class PaletteComponent implements OnInit {
+  public search!: string;
+  private errors!: string[];
+  public locked: boolean = false;
 
-  constructor(public paletteService: PaletteService) {}
+  constructor(
+    public paletteService: PaletteService,
+    private flowStructureService: FlowStructureService
+  ) {}
+
+  ngOnInit(): void {
+    this.getXmlParseErrors();
+  }
+
+  getXmlParseErrors(): void {
+    this.flowStructureService.errorObservable().subscribe({
+      next: (errors) => {
+        this.errors = errors;
+        this.locked = this.XmlErrorsFound();
+      },
+    });
+  }
+
+  XmlErrorsFound(): boolean {
+    return this.errors.length > 0;
+  }
 }
