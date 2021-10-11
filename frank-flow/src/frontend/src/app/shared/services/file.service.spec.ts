11aa347d79f9cc2384f69b3f905f62eb1cd4def1
Sergi Philipsen <philipsen.sergi@gmail.com>
2021-10-11 16:30:13 +0200

    Add toastr

--- frank-flow/src/frontend/src/app/shared/services/file.service.spec.ts
+++ frank-flow/src/frontend/src/app/shared/services/file.service.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed } from '@angular/core/testing';
-
+import { ToastrModule } from 'ngx-toastr';
 import { FileService } from './file.service';
 
 describe('FileService', () => {
   let service: FileService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [ToastrModule.forRoot()],
+    });
     service = TestBed.inject(FileService);
   });
 
--- frank-flow/src/frontend/src/app/shared/services/node-generator.service.spec.ts
+++ frank-flow/src/frontend/src/app/shared/services/node-generator.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { ToastrModule } from 'ngx-toastr';
 
 import { NodeGeneratorService } from './node-generator.service';
 
@@ -6,7 +7,9 @@ describe('NodeGeneratorService', () => {
   let service: NodeGeneratorService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [ToastrModule.forRoot()],
+    });
     service = TestBed.inject(NodeGeneratorService);
   });
 
