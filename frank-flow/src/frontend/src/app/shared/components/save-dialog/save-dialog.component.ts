d5445504507ab906e60fcdff6e497130b89df713
Ruben de Heer <38459075+chiefruds@users.noreply.github.com>
2021-11-08 15:23:31 +0100

    set flow update after mode change (#246)
    * set flow update after mode change
    
    * Remove getCurrentFile and change subscription to replaySubscriptions
    The replay always sends 1 version to new subscribers, so they don't need to ask for a initial version anymore
    
    * Remove log -- sorry Ruben...
    
    * DoNotUnbindJSPlumbInstanceEventListener
    
    * Check for currentFile
    
    Co-authored-by: Sergi Philipsen <philipsen.sergi@gmail.com>

--- frank-flow/src/frontend/src/app/shared/components/save-dialog/save-dialog.component.ts
+++ frank-flow/src/frontend/src/app/shared/components/save-dialog/save-dialog.component.ts
@@ -1,25 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { NgxSmartModalService } from 'ngx-smart-modal';
 import { CurrentFileService } from '../../services/current-file.service';
 import { File } from '../../models/file.model';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-save-dialog',
   templateUrl: './save-dialog.component.html',
   styleUrls: ['./save-dialog.component.scss'],
 })
-export class SaveDialogComponent {
+export class SaveDialogComponent implements OnInit, OnDestroy {
   item!: File;
   currentFile!: File | undefined;
+  private currentFileSubscription!: Subscription;
 
   constructor(
     private ngxSmartModalService: NgxSmartModalService,
     private currentFileService: CurrentFileService
   ) {}
 
+  ngOnInit() {
+    this.currentFileSubscription = this.currentFileService.currentFileObservable.subscribe(
+      (currentFile) => (this.currentFile = currentFile)
+    );
+  }
+
+  ngOnDestroy() {
+    this.currentFileSubscription.unsubscribe();
+  }
+
   onDataAdded(): void {
     this.item = this.ngxSmartModalService.getModalData('saveDialog');
-    this.currentFile = this.currentFileService.getCurrentFile();
   }
 
   save(): void {
