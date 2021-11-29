0bf4f708f43cd378bae4a978c2ac1bb096391f39
Sergi Philipsen <philipsen.sergi@gmail.com>
2021-11-29 12:32:03 +0100

    Fix click event on add-file button and check if directory is selected

--- frank-flow/src/frontend/src/app/header/header.component.ts
+++ frank-flow/src/frontend/src/app/header/header.component.ts
@@ -55,9 +55,15 @@ export class HeaderComponent implements OnInit {
   }
 
   openAddDialog(): void {
-    this.ngxSmartModalService
-      .getModal('addDialog')
-      .setData(this.currentFile, true)
-      .open();
+    const currentDirectory = this.currentFileService.currentDirectory;
+
+    if (currentDirectory?.configuration) {
+      this.ngxSmartModalService
+        .getModal('addDialog')
+        .setData(this.currentFile, true)
+        .open();
+    } else {
+      this.toastr.error('Please select a folder first', "Can't add item");
+    }
   }
 }
