47b7ba5bbef5aea3e69515fc9021bbe24f327994
Sergi Philipsen <philipsen.sergi@gmail.com>
2022-01-10 14:48:03 +0100

    refactor: move explorer out of the editor component

--- frank-flow/src/frontend/src/app/explorer/explorer.module.ts
+++ frank-flow/src/frontend/src/app/explorer/explorer.module.ts
@@ -1,15 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { EditorComponent } from './editor.component';
 import { FormsModule } from '@angular/forms';
-import { MonacoEditorComponent } from './monaco-editor/monaco-editor.component';
 import { SharedModule } from '../shared/components/shared.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { NgxSmartModalModule } from 'ngx-smart-modal';
+import { ExplorerComponent } from './explorer.component';
 
 @NgModule({
-  declarations: [EditorComponent, MonacoEditorComponent],
-  exports: [EditorComponent],
+  declarations: [ExplorerComponent],
+  exports: [ExplorerComponent],
   imports: [
     CommonModule,
     FormsModule,
@@ -18,4 +17,4 @@ import { NgxSmartModalModule } from 'ngx-smart-modal';
     NgxSmartModalModule,
   ],
 })
-export class EditorModule {}
+export class ExplorerModule {}
