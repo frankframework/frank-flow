3cda8c7cc6f3eef13f3916c87ec59564bf91a20f
Sergi Philipsen <philipsen.sergi@gmail.com>
2022-01-13 17:18:36 +0100

    refactor: move the floating actions into it's own component

---
+++ frank-flow/src/frontend/src/app/flow/actions/actions.component.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+import { PanZoomService } from '../../shared/services/pan-zoom.service';
+import {
+  faArrowDown,
+  faArrowLeft,
+  faArrowRight,
+  faArrowUp,
+  faDotCircle,
+  faHome,
+  faSearchMinus,
+  faSearchPlus,
+} from '@fortawesome/free-solid-svg-icons';
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+
+@Component({
+  selector: 'app-actions',
+  templateUrl: './actions.component.html',
+  styleUrls: ['./actions.component.scss'],
+})
+export class ActionsComponent {
+  constructor(
+    private library: FaIconLibrary,
+    private panZoomService: PanZoomService
+  ) {
+    this.library.addIcons(
+      faArrowDown,
+      faArrowUp,
+      faArrowRight,
+      faArrowLeft,
+      faSearchMinus,
+      faSearchPlus,
+      faHome,
+      faDotCircle
+    );
+  }
+
+  zoomIn(): void {
+    this.panZoomService.zoomIn();
+  }
+
+  zoomOut(): void {
+    this.panZoomService.zoomOut();
+  }
+
+  zoomReset(): void {
+    this.panZoomService.reset();
+  }
+
+  panCenter(): void {
+    this.panZoomService.panCenter();
+  }
+}
