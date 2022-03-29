9d2c13924448de4f1c8c228f3aa5b2ef09024547
Diede Kerkhof <diede@integrationpartners.nl>
2022-03-29 15:35:29 +0200

    fix: change text for delete confirm message

--- frank-flow/src/frontend/src/app/explorer/explorer.component.ts
+++ frank-flow/src/frontend/src/app/explorer/explorer.component.ts
@@ -64,8 +64,8 @@ export class ExplorerComponent {
       .getModal('confirmDialog')
       .setData(
         {
-          title: 'Are you sure',
-          text: `Do you want to delete this file from you directory? This action cannot be undone or reverted`,
+          title: 'Are you sure?',
+          text: `Do you want to delete ${this.currentFile.path} from ${this.currentFile.configuration}? This action cannot be undone or reverted!`,
           actionFunction: this.deleteFileFunction,
         },
         true
