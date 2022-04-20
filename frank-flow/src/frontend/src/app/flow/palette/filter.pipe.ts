9b3f8c0262bf928dea5ecccf7a7092d71e0445d5
Diede Kerkhof <diede@integrationpartners.nl>
2022-04-20 10:53:57 +0200

    fix: remove exits from palete and flow

--- frank-flow/src/frontend/src/app/flow/palette/filter.pipe.ts
+++ frank-flow/src/frontend/src/app/flow/palette/filter.pipe.ts
@@ -1,14 +1,18 @@
 import { Pipe, PipeTransform } from '@angular/core';
-
 @Pipe({
   name: 'filter',
 })
 export class FilterPipe implements PipeTransform {
-  transform(items: any[] | undefined, term: string): any[] | undefined {
+  transform(
+    items: any[] | undefined,
+    term: string,
+    not = false
+  ): any[] | undefined {
     return term
-      ? items?.filter((item: any) =>
-          item.name?.toLowerCase().includes(term.toLowerCase())
-        )
+      ? items?.filter((item: any) => {
+          const match = item.name?.toLowerCase().includes(term.toLowerCase());
+          return not ? !match : match;
+        })
       : items;
   }
 }
