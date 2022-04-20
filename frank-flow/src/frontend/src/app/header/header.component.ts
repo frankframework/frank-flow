d15dfb9bde766e61900f213bc13c2ed607af4e76
Diede Kerkhof <diede@integrationpartners.nl>
2022-04-20 09:25:07 +0200

    fix: remove the add file button from header

--- frank-flow/src/frontend/src/app/header/header.component.ts
+++ frank-flow/src/frontend/src/app/header/header.component.ts
@@ -60,19 +60,6 @@ export class HeaderComponent implements OnInit {
     this.ngxSmartModalService.getModal('configurationSettingsModal').open();
   }
 
-  openAddDialog(): void {
-    const currentDirectory = this.currentFileService.currentDirectory;
-
-    if (currentDirectory?.configuration) {
-      this.ngxSmartModalService
-        .getModal('addDialog')
-        .setData(this.currentFile, true)
-        .open();
-    } else {
-      this.toastr.error('Please select a folder first', "Can't add item");
-    }
-  }
-
   currentFileTitle() {
     if (this.currentFile?.type === FileType.EMPTY) {
       return 'No file selected';
